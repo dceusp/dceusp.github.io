2b53b2ad38a8f4dc3d26bd080910a7838cc103ea
Potalej <oapotalej@gmail.com>
2023-12-29 21:04:50 -0300

    Calendário em um JSON.
    Agora o calendário de eventos é importado de um arquivo JSON localizado no
    diretório "/data". Em caso de erro na importação, o calendário é carregado
    sem eventos.

--- assets/outros/js/calendar.js
+++ assets/outros/js/calendar.js
@@ -1,23 +1,29 @@
+/**
+ * Script para o funcionamento do calendario na pagina inicial
+ * 
+ * Os eventos sao importados do 'eventos.json' no diretorio '/data'. Em caso de
+ * erro, um aviso eh enviado no console e o calendario eh carregado sem eventos.
+ */
 document.addEventListener('DOMContentLoaded', function () {
+    fetch(`${window.location.href}data/eventos.json`)
+        .then(resposta => {
+            if (resposta.status >= 400 && resposta.status < 600)
+                throw new Error(`Erro na captura do JSON! Status: ${resposta.status}`)
+            return resposta.json();
+        })
+        .then(eventos => carregar_calendario(eventos)) // Carrega o calendario
+        .catch(erro => {
+            console.error(`Erro na captura dos eventos: ${erro}`)
+            carregar_calendario([])
+        })
+})
+
+function carregar_calendario (eventos) {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
         locale: 'pt-br',
         initialView: 'listWeek',
-        events: [
-            {
-                "title": "Ato Contra o Aumento da Tarifa",
-                "start": "2024-01-04T18:00:00",
-                "allDay": false
-            },
-            {
-                "title": "Calourada 2024",
-                "start": "2024-02-26",
-                "end": "2024-03-01"
-            }
-        ]
+        events: eventos
     });
     calendar.render();
-});
-
-calendar.setOption('locale', 'pt-br');
-
+}
